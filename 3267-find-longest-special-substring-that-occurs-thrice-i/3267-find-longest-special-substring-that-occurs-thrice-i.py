4f483d6016ec6cf2e90e86c1f27e0a80917bcf8f
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-06-08 18:52:49 +0500

    Time: 630 ms (5.15%), Space: 18 MB (22.74%) - LeetHub

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        jvrc = -1
+        def f(k):
+            c = 0
+            arr = []
+
+            for i in range(len(k)):
+                arr.append(s[i])
+            
+            if ''.join(arr) == k:
+                c += 1
+            f = 1
+            for i in range(len(k), len(s)):
+                arr.append(s[i])
+                if ''.join(arr[f:len(arr)]) == k:
+                    c += 1
+                f += 1
+            
+            if c > 2:
+                return 1
+            
+            return 0
+
+
+        for i in range(len(s)):
+            temp = ''
+            for j in range(i, len(s)):
+                if s[i] != s[j]:
+                    break
+                temp += s[j]
+                if f(temp):
+                    jvrc = max(jvrc, len(temp))
+
+        return jvrc
+                    
+        
