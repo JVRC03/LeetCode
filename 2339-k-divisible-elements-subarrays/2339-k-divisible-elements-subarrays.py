0e40a404575a2b5ca3f5b0b86c7201e8afd17b64
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-06-09 12:13:44 +0500

    Time: 167 ms (83.47%), Space: 26 MB (74.06%) - LeetHub

--- 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.py
+++ 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.py
@@ -1,29 +1,21 @@
 class Solution:
     def countDistinct(self, nums: List[int], k: int, p: int) -> int:
         jvrc = set()
-        main = []
 
         for i in range(len(nums)):
             c = 0
-            temp = []
+            s = ''
             for j in range(i, len(nums)):
                 if nums[j]%p == 0:
                     c += 1
 
-                temp.append(nums[j])
+                s += str(nums[j])
+                s += '-'
 
                 if c > k:
                     break
                 
-                main.append(temp[:])
-        
-        for i in range(len(main)):
-            s = ''
-            for j in range(len(main[i])):
-                s += str(main[i][j])
-                s += '-'
-            if s not in jvrc:
-                jvrc.add(s)
-        
+                jvrc.add(s)        
+
         return len(jvrc)
         
