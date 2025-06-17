f17186bfaf68c37216fbbc34a5ca71b48368e083
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-06-17 08:14:47 +0500

    Time: 693 ms (5.85%), Space: 29.6 MB (99.76%) - LeetHub

--- 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,25 +1,23 @@
 class Solution:
-    def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        p_sum = [nums[0]]
+    def getSumAbsoluteDifferences(self, jvrc: List[int]) -> List[int]:
+        p_sum = [jvrc[0]]
 
-        for i in range(1, len(nums)):
-            p_sum.append(p_sum[-1]+nums[i])
+        for i in range(1, len(jvrc)):
+            p_sum.append(p_sum[-1]+jvrc[i])
         
-        jvrc = []
-
-        for i in range(len(nums)):
+        for i in range(len(jvrc)):
             k = -1
             if i == 0:
-                k = abs(((len(nums)-1)*nums[i])-(p_sum[-1]-p_sum[0]))
-            elif i == len(nums)-1:
-                k = abs( ((len(nums)-1)*nums[i]) - (p_sum[-2]) )
+                k = abs(((len(jvrc)-1)*jvrc[i])-(p_sum[-1]-p_sum[0]))
+            elif i == len(jvrc)-1:
+                k = abs( ((len(jvrc)-1)*jvrc[i]) - (p_sum[-2]) )
             else:
-                r = abs(((len(nums)-i-1) * nums[i]) - (p_sum[-1]-p_sum[i]))
-                l = abs((i * nums[i]) - p_sum[i-1])
+                r = abs(((len(jvrc)-i-1) * jvrc[i]) - (p_sum[-1]-p_sum[i]))
+                l = abs((i * jvrc[i]) - p_sum[i-1])
 
                 k = (l+r)
 
-            jvrc.append(k)
+            jvrc[i] = k
         
         return jvrc
         
