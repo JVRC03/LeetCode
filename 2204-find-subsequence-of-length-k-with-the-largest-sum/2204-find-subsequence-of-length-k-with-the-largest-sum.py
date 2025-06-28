202d022a808c8dd316eb0486888a174604d98272
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-06-28 09:02:06 +0530

    Time: 2 ms (89.42%), Space: 17.8 MB (94.31%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,20 +1,19 @@
 class Solution:
-    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        arr = nums.copy()
-        nums.sort()
-        jvrc = []
+    def maxSubsequence(self, jvrc: List[int], k: int) -> List[int]:
+        arr = jvrc.copy()
+        jvrc.sort()
         dic = {}
-        c = 0
 
         while k:
             k -= 1
-            if nums[-1-c] not in dic:
-                dic[nums[-1-c]] = 1
+            if jvrc[-1] not in dic:
+                dic[jvrc[-1]] = 1
             else:
-                dic[nums[-1-c]] += 1
+                dic[jvrc[-1]] += 1
+            jvrc.pop()
 
-            c += 1
-        
+        jvrc = []
+
         for i in range(len(arr)):
             if arr[i] in dic and dic[arr[i]] > 0:
                 dic[arr[i]] -= 1
