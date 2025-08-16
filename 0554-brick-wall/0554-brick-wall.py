1bc7af82dfcffb4e62b7671e22acd7c424fca53e
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-08-16 14:04:02 +0530

    Time: 17 ms (11%), Space: 21.3 MB (98.98%) - LeetHub

---
+++ 0554-brick-wall/0554-brick-wall.py
@@ -0,0 +1,21 @@
+class Solution:
+    def leastBricks(self, arr: List[List[int]]) -> int:
+        dic = {}
+        jvrc = 0
+
+        for i in range(len(arr)):
+            curr = 0
+            for j in range(len(arr[i])):
+                curr += arr[i][j]
+                if curr not in dic:
+                    dic[curr] = 1
+                else:
+                    dic[curr] += 1
+                
+                if j != len(arr[i])-1:
+                    jvrc = max(jvrc, dic[curr])
+        
+        print(dic)
+        
+        return len(arr)-jvrc
+        
