5503bc0d0105f0eb1c5ad7ab7f56299304f17383
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-09-22 15:45:07 +0530

    Time: 0 ms (100%), Space: 17.9 MB (15.03%) - LeetHub

---
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        dic = {}
+        jvrc = 0
+        maxi = 0
+
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]] = 1
+            else:
+                dic[nums[i]] += 1
+        
+        for i in dic:
+            if dic[i] > maxi:
+                maxi = dic[i]
+                jvrc = dic[i]
+            elif dic[i] == maxi:
+                jvrc += dic[i]
+        
+        return jvrc
+        
