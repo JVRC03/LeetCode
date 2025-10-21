84e6572a6c63149eb64da7b5108ddfafda762b1f
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-10-21 19:46:55 +0530

    Time: 12 ms (41.41%), Space: 18.8 MB (53.52%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -1,35 +1,16 @@
 class Solution:
     def findChampion(self, n: int, arr: List[List[int]]) -> int:
-        v = [0] * n
-        dic = {}
+        s = set()
 
         for i in range(len(arr)):
-            if arr[i][0] not in dic:
-                dic[arr[i][0]] = [arr[i][-1]]
-            else:
-                dic[arr[i][0]].append(arr[i][-1])
+            s.add(arr[i][-1])
         
-        def dfs(source):
-            if v[source]:
-                return
-            
-            v[source] = 1
-            if source not in dic:
-                return
-            a = dic[source]
-
-            for i in range(len(a)):
-                dfs(a[i])
-
+        if len(s) != n-1:
+            return -1
+        
         for i in range(n):
-            v = [0] * n
-            dfs(i)
-            k = v.count(1)
-           
-            if k == n:
+            if i not in s:
                 return i
-    
-        return -1
-
+        
 
         
