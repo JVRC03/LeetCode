35907ac6695e7b4ec07ac344b1c003ebe47e53e2
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-10-26 21:16:26 +0530

    Time: 399 ms (5.56%), Space: 18 MB (88.87%) - LeetHub

--- 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.py
+++ 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.py
@@ -1,6 +1,5 @@
 class Solution:
     def minNumberOfFrogs(self, s: str) -> int:
-        temp = {}
         dic = {
             'c' : 0,
             'r' : 0,
@@ -11,42 +10,38 @@ class Solution:
         jvrc = 0
 
         for i in range(len(s)):
-            if s[i] not in temp:
-                temp[s[i]] = 1
-            else:
-                temp[s[i]] += 1
-        
-        if len(temp) != 5:
-            return -1
-        
-        c = temp['c']
-        for i in temp:
-            if temp[i] != c:
-                return -1
-        
-        for i in range(len(s)):
             if s[i] not in dic:
                 dic[s[i]] = 1
             else:
                 dic[s[i]] += 1
 
             arr = list(dic.values())
-            for i in range(len(arr)-1):
-                if arr[i] < arr[i+1]:
+            for j in range(len(arr)-1):
+                if arr[j] < arr[j+1]:
                     return -1
-                    
-            if len(dic) == 5:
-                yes = 1
 
+            yes = 1
+
+            for j in dic:
+                if dic[j] > 0:
+                    continue
+                yes = 0
+                break
+            if yes:
+                jvrc = max(jvrc, max(arr))
                 for j in dic:
-                    if dic[j] > 0:
-                        continue
-                    yes = 0
-                    break
-                if yes:
-                    jvrc = max(jvrc, max(dic.values()))
+                    dic[j] -= 1
+                if i == len(s)-1:
+                    c = dic['c']
                     for j in dic:
-                        dic[j] -= 1
+                        if dic[j] != c:
+                            return -1
+            else:
+                if i == len(s)-1:
+                    c = dic['c']
+                    for j in dic:
+                        if dic[j] != c:
+                            return -1
                     
         return jvrc
             
