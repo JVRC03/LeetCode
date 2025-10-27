422c8a3ed21c0df4cf9182250b4b4567df1b8111
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-10-27 13:26:16 +0530

    Time: 7 ms (72.71%), Space: 19.6 MB (25.73%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
@@ -1,16 +1,16 @@
 class Solution:
     def numberOfBeams(self, arr: List[str]) -> int:
-        temp = []
+        curr, jvrc = -1, 0
 
         for i in range(len(arr)):
             c = arr[i].count('1')
-            if c:
-                temp.append(c)
-        
-        jvrc = 0
-
-        for i in range(len(temp)-1):
-            jvrc += (temp[i] * temp[i+1])
+            if curr == -1:
+                if c:
+                    curr = c
+            else:
+                if c:
+                    jvrc += (c * curr)
+                    curr = c
 
         return jvrc
         
