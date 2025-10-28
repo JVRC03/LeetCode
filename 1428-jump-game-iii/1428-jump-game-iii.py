935838425c18ffcb910691bc588d4d29301399ff
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-10-28 21:26:25 +0530

    Time: 17 ms (24.11%), Space: 26.9 MB (42.67%) - LeetHub

---
+++ 1428-jump-game-iii/1428-jump-game-iii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canReach(self, arr: List[int], idx: int) -> bool:
+        if 0 not in arr:
+            return False
+        
+        k = set()
+
+        for i in range(len(arr)):
+            if arr[i] == 0:
+                k.add(i)
+
+        f = 0
+        s = {idx}
+        q = [[idx, arr[idx]]]
+
+        while f < len(q):
+            a = q[f][0]+q[f][1]
+            b = q[f][0]-q[f][1]
+
+            if a in k or b in k:
+                return True
+
+            if a < len(arr) and a not in s:
+                q.append([a, arr[a]])
+                s.add(a)
+            if b >= 0 and b not in s:
+                q.append([b, arr[b]])
+                s.add(b)
+            
+            f += 1
+        
+        return False
+            
+
+        
