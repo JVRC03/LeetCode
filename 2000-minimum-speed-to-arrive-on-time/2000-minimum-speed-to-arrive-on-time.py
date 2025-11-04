0c397a8e5509d2fbf42df939c396ce497cfbf509
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-11-04 22:05:55 +0530

    Time: 2407 ms (6.41%), Space: 28.8 MB (22.45%) - LeetHub

---
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minSpeedOnTime(self, arr: List[int], k: float) -> int:
+        if len(arr)-1 > k:
+            return -1
+        
+        f, r = 1, 10000000000
+        jvrc = float('inf')
+
+        def check(n):
+            ans = 0
+            for i in range(len(arr)-1):
+                ans += (int(math.ceil(arr[i]/n)))
+            
+            ans += (arr[-1]/n)
+
+            return ans
+
+        while f <= r:
+            mid = (f + r)//2
+            c = check(mid)
+
+            if c <= k:
+                r = mid-1
+                jvrc = min(jvrc, mid)
+            else:
+                f = mid+1
+        
+        if jvrc == float('inf'):
+            return -1
+        return jvrc
+
+
+
+
+
+
+
+        
