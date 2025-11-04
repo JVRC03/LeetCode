4230c76cb682fbd457772e77e116840f948f8519
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-11-04 22:06:49 +0530

    Time: 1692 ms (26.09%), Space: 28.6 MB (81.49%) - LeetHub

--- 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.py
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.py
@@ -3,7 +3,7 @@ class Solution:
         if len(arr)-1 > k:
             return -1
         
-        f, r = 1, 100000000
+        f, r = 1, 10000000
         jvrc = float('inf')
 
         def check(n):
