5aff2fe4bc0dadb3bde1e0dc1d55e9c9f597b25f
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-11-05 20:51:22 +0530

    Time: 174 ms (35.46%), Space: 24.4 MB (26.16%) - LeetHub

--- 3355-minimum-levels-to-gain-more-points/3355-minimum-levels-to-gain-more-points.py
+++ 3355-minimum-levels-to-gain-more-points/3355-minimum-levels-to-gain-more-points.py
@@ -13,7 +13,7 @@ class Solution:
             else:
                 p_sum.append(p_sum[-1]-1)
         
-        jvrc = float('inf')
+        jvrc = -1
         count = float('-inf')
 
         for i in range(1, len(arr)):
@@ -21,17 +21,13 @@ class Solution:
             b = p_sum[-1]-p_sum[i-1]
 
             if a > b:
-                if count < a:
-                    count = a
-                    jvrc = min(i, jvrc)
+                jvrc = i
+                break
            
         if len(arr) == 2:
             if arr[0] > arr[-1]:
                 return 1
             return -1
-        
-        if jvrc == float('inf'):
-            jvrc = -1
 
         return jvrc
 
