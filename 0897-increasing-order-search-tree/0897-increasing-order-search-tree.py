4cbf581748cba39cf6dcbbc3563cb00da65a3098
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-11-27 13:56:04 +0530

    Time: 0 ms (100%), Space: 18 MB (16.4%) - LeetHub

---
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.jvrc = None
+        self.temp = None
+    
+    def dfs(self, root):
+        if root is None:
+            return 
+        
+        self.dfs(root.left)
+        node = TreeNode()
+        node.val = root.val
+        node.left = None
+        node.right = None
+
+        if self.jvrc == None:
+            self.jvrc = node
+            self.temp = node
+        else:
+            self.temp.right = node
+            self.temp = node
+        
+        self.dfs(root.right)
+
+    def increasingBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.dfs(root)
+
+        return self.jvrc
+
+        
