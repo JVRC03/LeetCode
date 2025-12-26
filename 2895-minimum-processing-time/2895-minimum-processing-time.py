2011dcb98e934c98316b7b23b4304154b690cf5f
~JVRC <140805892+JVRC03@users.noreply.github.com>
2025-12-26 20:06:33 +0530

    Time: 76 ms (51.95%), Space: 27.8 MB (100%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.py
+++ 2895-minimum-processing-time/2895-minimum-processing-time.py
@@ -1,14 +1,13 @@
 class Solution:
     def minProcessingTime(self, a: List[int], arr: List[int]) -> int:
-        jvrc = 0
+        jvrc, f = 0, 0
         a.sort()
-        a = a[::-1]
-
+    
         arr.sort()
 
         while len(arr):
-            k = a[-1]+arr[-1]
-            a.pop()
+            k = a[f]+arr[-1]
+            f += 1
             arr.pop()
             arr.pop()
             arr.pop()
