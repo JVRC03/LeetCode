0bd9881c145bb62d1298e362d6ee0893a47f53ca
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-01-17 14:50:03 +0530

    Time: 2349 ms (5.02%), Space: 32 MB (15.05%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumTime(self, arr: List[int], k: int) -> int:
+        low, high = 1, max(arr) * 100000000009
+        jvrc = float('inf')
+
+        def check(arr, n):
+            tot = 0
+
+            for i in range(len(arr)):
+                tot += (n//arr[i])
+            
+            return tot
+
+        while low <= high:
+            mid = low + ((high - low) // 2)
+
+            ans = check(arr, mid)
+
+            if ans >= k:
+                jvrc = min(jvrc, mid)
+                high = mid-1
+            else:
+                low = mid+1
+
+        return jvrc
+        
