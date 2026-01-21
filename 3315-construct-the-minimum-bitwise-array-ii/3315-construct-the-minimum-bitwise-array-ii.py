31d283e63d2703294da6b3c0802d38a22fdf2af6
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-01-21 08:15:20 +0530

    Time: 9 ms (22.64%), Space: 19.6 MB (13.21%) - LeetHub

---
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        jvrc = []
+
+        for i in range(len(nums)):
+            s = bin(nums[i])
+            s = s[2:]
+            arr = list(s)
+
+            if arr[-1] == '0':
+                jvrc.append(-1)
+            else:
+                idx = -1
+
+                for j in range(len(arr)-1, -1, -1):
+                    if arr[j] == '0':
+                        break
+                    idx = j
+                
+                arr[idx] = '0'
+            
+                temp = ''.join(arr)
+                k = int(temp, 2)
+                jvrc.append(k)
+        
+        return jvrc 
+                
+        
