3f85dcbe3d94c1669a7de6de5506ecdffaec9de1
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-01-31 23:08:05 +0530

    Time: 1945 ms (14.08%), Space: 20.2 MB (8.6%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,29 @@
+class Solution:
+    def f(self, nums, idx, dp, curr):
+        if idx >= len(nums):
+            return 0
+        
+        if dp[idx] != -1:
+            return dp[idx]
+        
+        glob = 1
+
+        for i in range(idx, len(nums)):
+            if nums[i] > curr:
+                k = 1+self.f(nums, i, dp, nums[i])
+                glob = max(k, glob)
+        
+        dp[idx] = glob
+
+        return dp[idx]
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        jvrc = 0
+        dp = [-1] * len(nums)
+
+        for i in range(len(nums)):
+            val = self.f(nums, i, dp, nums[i])
+            jvrc = max(jvrc, val)
+
+        return jvrc
+        
