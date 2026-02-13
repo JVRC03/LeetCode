acdaf9c71742a62f38946f86b205dd9fc1fce9f3
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-02-13 07:48:31 +0530

    Time: 12 ms (32.6%), Space: 19.7 MB (14.27%) - LeetHub

--- 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.py
+++ 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.py
@@ -1,22 +1,23 @@
 class Solution:
 
-    def get(self, k):
-        s = bin(k)
-        s = s[2:]
-
-        s = s[::-1]
-
-        return [int(s, 2), k]
-
     def sortByReflection(self, nums: List[int]) -> List[int]:
         jvrc = []
-        c = []
-        for i in range(len(nums)):
-            c.append(self.get(nums[i]))
-        
-        c.sort()
-        for i in range(len(c)):
-            jvrc.append(c[i][-1])
+        heap = []
 
+        heapq.heapify(heap)
+
+        for i in range(len(nums)):
+            s = bin(nums[i])
+            s = s[2:]
+            s = s[::-1]
+            k = int(s, 2)
+
+            heapq.heappush(heap, [k, nums[i]])
+        
+        while len(heap):
+            a = heapq.heappop(heap)
+            jvrc.append(a[-1])
+        
         return jvrc
+        
         
