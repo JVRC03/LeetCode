c5900e00b7c9b59a4a7c7cb17a8aa5f7cf36546b
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-03-07 16:00:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,61 +1,48 @@
 class Solution:
     def orangesRotting(self, mat: List[List[int]]) -> int:
         q = deque([])
-        self.jvrc = 0
-        rott, not_rott = 0, 0
+        not_rot, jvrc = 0, 0
 
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j] == 2:
                     q.append([i, j])
-                    rott += 1
                 elif mat[i][j] == 1:
-                    not_rott += 1
+                    not_rot += 1
         
-        if rott == 0 and not_rott == 0:
+        if not_rot == 0:
             return 0
-        
-        if rott == 0:
+        if not len(q):
             return -1
-        
-        if not_rott == 0:
-            return 0
         
         while len(q):
             c = len(q)
 
-            for i in range(c):
-                n = q.popleft()
+            for idx in range(c):
+                pair = q.popleft()
 
-                a, b = n[0], n[1]
+                i, j = pair[0], pair[1]
 
-                if a < len(mat) and b+1 < len(mat[0]) and mat[a][b+1] == 1:
-                    q.append([a, b+1])
-                    mat[a][b+1] = 2
+                if j+1 < len(mat[0]) and mat[i][j+1] == 1:
+                    q.append([i, j+1])
+                    mat[i][j+1] = 0
+                
+                if j-1 > -1 and mat[i][j-1] == 1:
+                    q.append([i, j-1])
+                    mat[i][j-1] = 0
 
-                if a < len(mat) and b-1 > -1 and mat[a][b-1] == 1:
-                    q.append([a, b-1])
-                    mat[a][b-1] = 2
-                
-                if a+1 < len(mat) and b < len(mat[0]) and mat[a+1][b] == 1:
-                    q.append([a+1, b])
-                    mat[a+1][b] = 2
-                
-                if a-1 > -1 and b < len(mat[0]) and mat[a-1][b] == 1:
-                    q.append([a-1, b])
-                    mat[a-1][b] = 2
-            
-            if not len(q):
-                break
-            self.jvrc += 1
-                
+                if i+1 < len(mat) and mat[i+1][j] == 1:
+                    q.append([i+1, j])
+                    mat[i+1][j] = 0
+
+                if i-1 > -1 and mat[i-1][j] == 1:
+                    q.append([i-1, j])
+                    mat[i-1][j] = 0
+            if len(q):
+                jvrc += 1      
+
         for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if mat[i][j] == 1:
-                    return -1
+            if 1 in mat[i]:
+                return -1  
         
-        return self.jvrc
-
-
-
-        
+        return jvrc
