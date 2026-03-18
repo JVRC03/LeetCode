4f46283613f69d27ebf53eae759a06dec732d54d
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-03-18 14:17:47 +0530

    Time: 223 ms (89.65%), Space: 67.1 MB (35.17%) - LeetHub

---
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countSubmatrices(self, mat: List[List[int]], target: int) -> int:
+        jvrc = 0       
+        top = []
+
+        for i in range(len(mat)):
+            temp = []
+            local = 0
+            for j in range(len(mat[0])):
+                local += mat[i][j]
+                val = -1
+
+                if len(top):
+                    val = local + top[i-1][j]
+                else:
+                    val = local
+
+                temp.append(val)
+
+                if val <= target:
+                    jvrc += 1
+                    
+            top.append(temp)
+                
+        return jvrc
