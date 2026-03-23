e92b0a973524b2f237fdb42fb30b88ef91511668
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-03-23 16:06:22 +0530

    Time: 1327 ms (5.53%), Space: 32 MB (61.68%) - LeetHub

--- 3639-minimum-time-to-activate-string/3639-minimum-time-to-activate-string.py
+++ 3639-minimum-time-to-activate-string/3639-minimum-time-to-activate-string.py
@@ -2,25 +2,27 @@ class Solution:
     def minTime(self, s: str, arr: List[int], k: int) -> int:
         f, r = 0, len(s)
         jvrc = float('inf')
+        self.vis = [0] * len(arr)
+        self.leng = len(arr)
 
         def check(idx, arr, k):
             tot = 0
-            vis = [0] * len(arr)
-
+            
             for i in range(idx):
-                vis[arr[i]] = 1
+                self.vis[arr[i]] = 1
             
             temp = []
-            for i in range(len(vis)):
-                if vis[i]:
+            for i in range(self.leng):
+                if self.vis[i]:
                     temp.append(i)
+                    self.vis[i] = 0
 
             for i in range(idx):
                 if i == 0:
-                    left, right = temp[i]+1, len(arr)-temp[i]
+                    left, right = temp[i]+1, self.leng-temp[i]
                     tot += (left * right)
                 else:
-                    left, right = (temp[i] - temp[i-1]), len(arr) - temp[i]
+                    left, right = (temp[i] - temp[i-1]), self.leng - temp[i]
                     tot += (left * right)
             
             if tot >= k:
