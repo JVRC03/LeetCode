ae0287ff0790318d2973146e7d7cb3571dd37fd7
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-04-09 08:54:48 +0530

    Time: 0 ms (100%), Space: 19.2 MB (69.15%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,6 @@
 class Solution:
     def plusOne(self, arr: List[int]) -> List[int]:
-        r = len(arr)-1
+        r = len(arr) - 1
 
         while r >= 0:
             if arr[r] != 9:
@@ -10,10 +10,10 @@ class Solution:
             arr[r] = 0
             r -= 1
         
-        if r < 0:
+        if r == -1:
             arr[0] = 1
             arr.append(0)
         
         return arr
-
+        
         
