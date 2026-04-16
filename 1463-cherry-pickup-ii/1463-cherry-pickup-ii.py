560fdfae1b73f71af6a345cf20c6acb7832820ea
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-04-16 20:30:29 +0530

    Time: 1073 ms (40.69%), Space: 30.9 MB (38.24%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -3,13 +3,13 @@ class Solution:
         dp = []
 
         for i in range(len(mat)):
-            curr = []
+            temp = []
             for j in range(len(mat[0])):
-                temp = []
+                curr = []
                 for k in range(len(mat[0])):
-                    temp.append(-1)
-                curr.append(temp)
-            dp.append(curr)
+                    curr.append(-1)
+                temp.append(curr)
+            dp.append(temp)
         
         def func(i, j1, j2, mat):
             if j1 < 0 or j1 >= len(mat[0]) or j2 < 0 or j2 >= len(mat[0]):
@@ -17,28 +17,28 @@ class Solution:
             
             if i == len(mat)-1:
                 if j1 == j2:
-                    return mat[i][j1]
+                    return mat[i][j2]
                 return mat[i][j1] + mat[i][j2]
-            
+        
             if dp[i][j1][j2] != -1:
                 return dp[i][j1][j2]
-
+            
             maxi = 0
-
             for x in range(-1, 2, 1):
                 for y in range(-1, 2, 1):
                     val = 0
+
                     if j1 == j2:
                         val = mat[i][j1]
                     else:
                         val = mat[i][j1] + mat[i][j2]
                     
-                    val += func(i+1, j1+x, j2+y, mat)
+                    val += func(i+1, j1 + x, j2 + y, mat)
+                
                     maxi = max(maxi, val)
 
             dp[i][j1][j2] = maxi
-
-            return dp[i][j1][j2]
-
-        return func(0, 0, len(mat[0]) - 1, mat)
+            return maxi
+            
+        return func(0, 0, len(mat[0])-1, mat)
         
