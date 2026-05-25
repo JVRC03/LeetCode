a90eff1f7528798568f419024c07ada26b26f198
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-05-25 12:33:00 +0530

    Time: 3 ms (20%), Space: 19.2 MB (20%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
@@ -1,16 +1,26 @@
 class Solution:
     def limitOccurrences(self, nums: list[int], k: int) -> list[int]:
-        jvrc, dic = [], {}
+        jvrc, curr, val = [], -1, -1
 
-        for i in range(len(nums)):
-            if nums[i] not in dic:
-                dic[nums[i]] = 1
+        i = 0
+        while i < len(nums):
+            if val == -1:
+                val = nums[i]
+                curr = 1
+                if curr <= k:
+                    jvrc.append(val)
+                i += 1
             else:
-                dic[nums[i]] += 1
-            
-            if dic[nums[i]] <= k:
-                jvrc.append(nums[i])
-        
+                if val == nums[i]:
+                    curr += 1
+                else:
+                    val = nums[i]
+                    curr = 1
+                
+                if curr <= k:
+                    jvrc.append(val)
+                i += 1
+                
         return jvrc
 
 
