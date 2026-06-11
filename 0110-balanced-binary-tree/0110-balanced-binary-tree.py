19a7967598eff958cb18562ef3358c077ffbb3b1
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-06-11 13:17:13 +0530

    Time: 0 ms (100%), Space: 20.6 MB (18.05%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,23 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.jvrc = True
-    
-    def dfs(self, root):
-        if root is None:
-            return 0
-        
-        left = self.dfs(root.left)
-        right = self.dfs(root.right)
-
-        if abs(left - right) > 1:
-            self.jvrc = False
-        
-        return 1 + max(left, right)
-
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.dfs(root)
+        self.jvrc = True
+
+        def dfs(root):
+            if root is None or self.jvrc == False:
+                return 0
+            
+            l = dfs(root.left)
+            r = dfs(root.right)
+
+            if not self.jvrc:
+                return 
+
+            if abs(l - r) > 1:
+                self.jvrc = False
+                return 
+            
+            return max(l, r) + 1
+
+        dfs(root)
 
         return self.jvrc
         
