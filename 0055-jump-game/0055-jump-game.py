b841a66202d90bd678784c2d584ded31b70c3129
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-07-13 15:38:55 +0530

    Time: 19 ms (62.92%), Space: 20.4 MB (30.98%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        flag = len(nums) - 1
+
+        for i in range(len(nums)-1, -1, -1):
+            if i + nums[i] >= flag:
+                flag = i
+
+        if flag != 0:
+            return False
+
+        return True            
+        
+        
