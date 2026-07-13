bb082d7762bfe62e6c609c3e934db0996b4a1dae
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-07-13 15:27:56 +0530

    Time: 4 ms (61.3%), Space: 19.3 MB (25.52%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,36 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        u = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
-        t = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
-        h = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
-
-        jvrc = ''
+        u = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+        t = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+        h = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        to = ['', 'M', 'MM', 'MMM']
 
         k = str(num)
 
-        if len(k) == 4:
-            jvrc += (int(k[0]) * 'M')
-            if k[1] != '0':
-                jvrc += (h[int(k[1]) - 1])
-            if k[2] != '0':
-                jvrc += (t[int(k[2]) - 1])
-            if k[3] != '0':
-                jvrc += (u[int(k[3]) - 1])
-        
-        elif len(k) == 3:
-            jvrc += (h[int(k[0]) - 1])
-            if k[1] != '0':
-                jvrc += (t[int(k[1]) - 1])
-            if k[2] != '0':
-                jvrc += (u[int(k[2]) - 1])
-        
-        elif len(k) == 2:
-            jvrc += (t[int(k[0]) - 1])
-            if k[1] != '0':
-                jvrc += (u[int(k[1]) - 1])
+        diff = 4 - len(k)
 
-        else:
-            jvrc += (u[int(k[0]) - 1])
+        k = (diff * '0') + k
+
+        jvrc = to[int(k[0])] + h[int(k[1])] + t[int(k[2])] + u[int(k[3])]
         
         return jvrc
         
