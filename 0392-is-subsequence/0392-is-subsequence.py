460ced751fde42f7f197f7c6700db558e9c2e3dd
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-07-15 11:30:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (64.45%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,12 +4,14 @@ class Solution:
 
         while f < len(s) and r < len(t):
             if s[f] == t[r]:
-                f += 1    
+                f += 1
+                r += 1
+                continue
+            
             r += 1
         
-        if f >= len(s):
+        if f == len(s):
             return True
         
         return False
-        
         
