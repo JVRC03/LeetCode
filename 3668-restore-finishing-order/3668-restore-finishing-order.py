9b3ac4b230ed6e298c694df805eb4bf1fc78043b
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-08-07 08:39:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (19.93%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.py
+++ 3668-restore-finishing-order/3668-restore-finishing-order.py
@@ -1,9 +1,14 @@
 class Solution:
-    def recoverOrder(self, arr: List[int], jvrc: List[int]) -> List[int]:
-        k = set(jvrc)
-        jvrc = []
-        for i in range(len(arr)):
-            if arr[i] in k:
-                jvrc.append(arr[i])
+    def recoverOrder(self, a: List[int], b: List[int]) -> List[int]:
+        s = set(b)
+        f = 0
+
+        for i in range(len(a)):
+            if f == len(b):
+                break
+            if a[i] in s:
+                b[f] = a[i]
+                f += 1
         
-        return jvrc
+        return b
+        
