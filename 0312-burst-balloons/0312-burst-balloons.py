caf510bf57eef1356e5f19f7cca6e23bfe3aaab7
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-08-08 20:46:29 +0530

    Time: 4224 ms (36.19%), Space: 27.1 MB (49.54%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxCoins(self, arr: List[int]) -> int:
+        arr.append(1)
+        arr.insert(0, 1)
+
+        dp = []
+
+        for i in range(len(arr) + 2):
+            temp = [-1] * (len(arr) + 2)
+            dp.append(temp)
+
+        def func(i, j, arr):
+            
+            if i > j:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            maxi = 0
+            for k in range(i, j+1):
+                steps = (arr[i - 1] * arr[k] * arr[j + 1]) + func(i, k-1, arr) + func(k+1, j, arr)
+                
+                maxi = max(maxi, steps)
+
+            dp[i][j] = maxi
+            return dp[i][j]
+
+        return func(1, len(arr) - 2, arr)
+        
