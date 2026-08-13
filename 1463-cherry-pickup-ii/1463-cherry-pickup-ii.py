5501d3400457e317d4bfb96da209dd4763079019
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-08-13 15:49:52 +0530

    Time: 840 ms (63.98%), Space: 30.7 MB (44.37%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,44 +1,41 @@
 class Solution:
     def cherryPickup(self, mat: List[List[int]]) -> int:
-        dp = []
+        dp  = []
 
         for i in range(len(mat)):
             temp = []
             for j in range(len(mat[0])):
-                curr = []
-                for k in range(len(mat[0])):
-                    curr.append(-1)
-                temp.append(curr)
+                temp.append([-1] * len(mat[0]))
             dp.append(temp)
-        
+
         def func(i, j1, j2, mat):
-            if j1 < 0 or j1 >= len(mat[0]) or j2 < 0 or j2 >= len(mat[0]):
+            if i >= len(mat) or j1 < 0 or j1 >= len(mat[0]) or j2 < 0 or j2 >= len(mat[0]):
                 return 0
-            
-            if i == len(mat)-1:
-                if j1 == j2:
-                    return mat[i][j2]
-                return mat[i][j1] + mat[i][j2]
-        
+
             if dp[i][j1][j2] != -1:
                 return dp[i][j1][j2]
-            
-            maxi = 0
-            for x in range(-1, 2, 1):
-                for y in range(-1, 2, 1):
-                    val = 0
 
-                    if j1 == j2:
-                        val = mat[i][j1]
-                    else:
-                        val = mat[i][j1] + mat[i][j2]
-                    
-                    val += func(i+1, j1 + x, j2 + y, mat)
-                
-                    maxi = max(maxi, val)
-
-            dp[i][j1][j2] = maxi
-            return maxi
+            val = -1
+            if j1 == j2:
+                val = mat[i][j1]
+            else:
+                val = mat[i][j1] + mat[i][j2]
             
-        return func(0, 0, len(mat[0])-1, mat)
+            a = func(i + 1, j1 - 1, j2 - 1, mat)
+            b = func(i + 1, j1 - 1, j2, mat)
+            c = func(i + 1, j1 - 1, j2 + 1, mat)
+
+            d = func(i + 1, j1, j2 - 1, mat)
+            e = func(i + 1, j1, j2, mat)
+            f = func(i + 1, j1, j2 + 1, mat)
+
+            g = func(i + 1, j1 + 1, j2 - 1, mat)
+            h = func(i + 1, j1 + 1, j2, mat)
+            fin = func(i + 1, j1 + 1, j2 + 1, mat)
+
+            dp[i][j1][j2] = val + max(a, b, c, d, e, f, g, h, fin)
+
+            return dp[i][j1][j2]
+
+        return func(0, 0, len(mat[0]) - 1, mat)
         
