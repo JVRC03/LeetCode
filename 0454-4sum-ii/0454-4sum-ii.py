38cc51642abe0430a953b38c61e3232cfc016e7b
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-08-22 10:06:05 +0530

    Time: 465 ms (15.78%), Space: 19.4 MB (81.28%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,27 +1,25 @@
 class Solution:
-    def fourSumCount(self, n1: List[int], n2: List[int], n3: List[int], n4: List[int]) -> int:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         dic = {}
 
-        for i in range(len(n3)):
-            for j in range(len(n4)):
-                k = n3[i]+n4[j]
-                if k not in dic:
-                    dic[k] = 1
+        for i in range(len(nums3)):
+            for j in range(len(nums4)):
+                curr = nums3[i] + nums4[j]
+
+                if curr not in dic:
+                    dic[curr] = 1
                 else:
-                    dic[k] += 1
-
+                    dic[curr] += 1
+            
         jvrc = 0
-        for i in range(len(n1)):
-            for j in range(len(n2)):
-                temp = n1[i]+n2[j]
-                if -temp in dic:
-                    jvrc += dic[-temp]
-                
-        return jvrc                           
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                curr = nums1[i] + nums2[j]
 
+                diff = -curr
 
-
-
-
+                if diff in dic:
+                    jvrc += dic[diff]
+        
         return jvrc
         
