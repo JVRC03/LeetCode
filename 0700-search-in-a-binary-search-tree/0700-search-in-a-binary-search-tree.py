f9915d926187b7a4ef4e95737c39daa99b055ac2
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-09-01 19:39:29 +0530

    Time: 6 ms (2.08%), Space: 20.9 MB (40.64%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -5,24 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.jvrc = None
-    
-    def dfs(self, root, k):
-        if (root is None) or (self.jvrc is not None):
-            return 
-        
-        if root.val == k:
-            self.jvrc = root
-            return
-        
-        if root.val > k:
-            self.dfs(root.left, k)
-        else:
-            self.dfs(root.right, k)
-        
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        self.dfs(root, val)
+        self.jvrc = None
 
+        def dfs(root, k):
+            if root is None or self.jvrc is not None:
+                return 0
+            
+            if root.val == k:
+                self.jvrc = root
+                return 0
+            
+            return dfs(root.left, k) | dfs(root.right, k)
+
+        dfs(root, val)
         return self.jvrc
         
