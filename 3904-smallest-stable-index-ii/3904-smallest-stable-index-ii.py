4b5cd285cbabbd73ca56d33d3272bba3f2eba841
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-09-05 09:13:30 +0530

    Time: 136 ms (88.64%), Space: 31.7 MB (84.09%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -1,19 +1,21 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-        p_sum = [nums[-1]]
+        pre, curr = [], float('inf')
 
-        for i in range(len(nums)-2, -1, -1):
-            p_sum.append(min(nums[i], p_sum[-1]))
-        p_sum = p_sum[::-1]
+        for i in range(len(nums)-1, -1, -1):
+            curr = min(curr, nums[i])
+            pre.append(curr)
         
-        curr = 0
+        pre = pre[::-1]
+
+        curr = -1
         for i in range(len(nums)):
             curr = max(curr, nums[i])
-
-            diff = curr - p_sum[i]
-
-            if diff <= k:
+            if curr - pre[i] <= k:
                 return i
-
+        
         return -1
+
+
+
         
