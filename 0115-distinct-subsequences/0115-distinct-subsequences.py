864d2d0fdaef635147533f821582b03132a61c95
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-09-06 09:50:03 +0530

    Time: 435 ms (42.7%), Space: 43.8 MB (63.47%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,20 +1,30 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         dp = []
+        for i in range(len(s)):
+            dp.append([-1] * len(t))
 
-        for i in range(len(s) + 1):
-            temp = [0] * (len(t) + 1)
-            dp.append(temp)
-        
-        for i in range(len(dp)):
-            dp[i][0] = 1
-        
-        for i in range(1, len(dp)):
-            for j in range(1, len(dp[0])):
-                if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j]
-        
-        return dp[-1][-1]
+        def func(i, j, s, t):
+            if i < 0 and j < 0:
+                return 1
+            
+            if i < 0:
+                return 0
+
+            if j < 0:
+                return 1
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            val = 0
+            if s[i] == t[j]:
+                val = func(i - 1, j, s, t) + func(i - 1, j - 1, s, t)
+            else:
+                val = func(i - 1, j, s, t)
+            
+            dp[i][j] = val
+            return dp[i][j]
+
+        return func(len(s) - 1, len(t) - 1, s, t)
         
