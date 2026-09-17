f970ebe10c7de2b671d5f90ba34953a167aec3d1
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-09-17 14:00:32 +0530

    Time: 72 ms (13.12%), Space: 28.6 MB (76.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,24 +1,12 @@
 class Solution:
-    def maxProfit(self, arr: List[int]) -> int:
-        temp = [0] * len(arr)
-        temp[-1] = 0
-
-        curr = arr[-1]
-
-        for i in range(len(arr)-2, -1, -1):
-            if curr <= arr[i]:
-                temp[i] = 0
-            else:
-                temp[i] = curr
-            
-            curr = max(curr, arr[i])
-        
+    def maxProfit(self, arr: list[int]) -> int:
         jvrc = 0
-        print(temp)
+        curr = 0
 
-        for i in range(len(arr)):
-            if temp[i]:
-                jvrc = max(jvrc, abs(temp[i] - arr[i]))
+        for i in range(len(arr) - 1, -1, -1):
+            curr = max(curr, arr[i])
+
+            jvrc = max(jvrc, curr - arr[i])
         
         return jvrc
         
