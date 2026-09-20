38b5b5e76e8476485c1cc81fe2f7f8d930da5b30
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-09-20 18:53:44 +0530

    Time: 13 ms (88.86%), Space: 19.4 MB (33.41%) - LeetHub

---
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countIntersectingIntervals(self, arr: list[list[int]]):
+        arr.sort()
+        jvrc = 0
+
+        def func(k, ff, arr):
+            f, r = ff, len(arr) - 1
+            ans = -1
+
+            while f <= r:
+                mid = f + ((r - f) // 2)
+
+                if arr[mid][0] <= k:
+                    f = mid + 1
+                    ans = mid
+                else:
+                    r = mid - 1
+
+            return ans
+
+        for i in range(len(arr)):
+            idx = func(arr[i][1], i + 1, arr)
+            if idx > 0:
+                jvrc += (idx - i)
+
+        return jvrc
+        
